e10de7837cdc7c23377a82d0e5cd0c183c5d5baf
Ben Moore <b3nmoore@gmail.com>
2017-01-18 19:39:41 +0000

    add scrapy script

---
+++ python/sharp_scrape.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.6
+
+import scrapy
+
+ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
+FILES_URLS_FIELD = 'image'
+IMAGES_STORE = 'data/images'
+
+class LidSpider(scrapy.Spider):
+    """ Scraper for sharp helmet ratings """
+
+    def start_requests(self):
+        urls = ['https://sharp.dft.gov.uk/testhelmetlist?page=%d&sharp-make=All&sharp-model=&sharp-type=All&sharp-rating=1&discontinued=1' % i for i in range(0, 41)]
+        for url in urls:
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        rows = response.xpath('//table[@id="search-results"]/tbody/tr')
+        for row in rows:
+            yield {
+                'name': row.xpath('./td[contains(@class, "views-field-title-1")]/a/text()').extract(),
+                'type': row.xpath('./td[contains(@class, "views-field-name")]/text()').extract(),
+                'image': row.xpath('./td[contains(@class, "views-field-field-helmet-image-fid-1")]/img/@src').extract(),
+                'price': row.xpath('./td[contains(@class, "views-field-field-price-value")]/text()').extract(),
+                'size_range': row.xpath('./td[contains(@class, "views-field-tid")]/text()').extract()
+            }
+

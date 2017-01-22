e41928d35eae339716c716e0cdaa5535722a41ae
Ben Moore <b3nmoore@gmail.com>
2017-01-22 21:43:04 +0000

    add ng material components

--- showy/src/app/app.module.ts
+++ showy/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
+import { DataSelectComponent } from './data-select/data-select.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataSelectComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
